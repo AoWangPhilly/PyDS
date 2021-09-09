fe51556e997c8c395ba7900ee1732fccb59a60e5
AoWangPhilly <aowangphilly@gmail.com>
2021-09-09 18:48:29 -0400

    Add documentation

--- src/PyDS/Stack.py
+++ src/PyDS/Stack.py
@@ -5,17 +5,33 @@ from PyDS.Error import Empty
 
 @dataclass
 class Stack:
+    """Implementation of Stack ADT"""
     __list: list = field(default_factory=list)
 
     def push(self, value: Any) -> None:
+        """Inserts value to the top of stack
+
+        :param value: A value to insert
+        :type value: Any
+        """
         self.__list.append(value)
 
     def pop(self) -> Any:
+        """Deletes value from top of stack
+
+        :return: A value from the top of the list
+        :rtype: Any
+        """
         if self.is_empty():
             raise Empty('Stack is empty')
         return self.__list.pop()
 
     def top(self) -> Any:
+        """Grab top value of stack without deletion
+
+        :return: A value from the top of the list
+        :rtype: Any
+        """
         if self.is_empty():
             raise Empty('Stack is empty')
         return self.__list[-1]
