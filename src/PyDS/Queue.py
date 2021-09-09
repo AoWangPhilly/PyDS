0bb4f5d48e3246802b55a4ebd5919daf1a63d366
AoWangPhilly <aowangphilly@gmail.com>
2021-09-09 13:00:20 -0400

    Add tests for queue

--- src/PyDS/Queue.py
+++ src/PyDS/Queue.py
@@ -1,6 +1,7 @@
 from typing import Any
 from dataclasses import dataclass, field
-from Error import Empty
+from PyDS.Error import Empty
+
 
 @dataclass
 class Queue:
@@ -8,26 +9,23 @@ class Queue:
 
     def enqueue(self, value: Any) -> None:
         self.__list.append(value)
-    
+
     def dequeue(self) -> Any:
+        if self.is_empty():
+            raise Empty("Queue is empty")
         return self.__list.pop(0)
-    
+
     def front(self) -> Any:
+        if self.is_empty():
+            raise Empty("Queue is empty")
         return self.__list[0]
 
     def is_empty(self) -> bool:
         return self.__len__() == 0
-    
+
     def __len__(self) -> int:
         return len(self.__list)
-    
+
     def __str__(self) -> str:
         return f'Queue({str(self.__list)})'
 
-if __name__ == '__main__':
-    q = Queue()
-    
-    for i in range(10):
-        q.enqueue(i)
-    
-    print(q)
---
+++ tests/test_queue.py
@@ -0,0 +1,80 @@
+import pytest
+from PyDS.Queue import Queue
+from PyDS.Error import Empty
+
+NUM_OF_ELEMENTS = 10
+
+
+@pytest.fixture()
+def queue():
+    q = Queue()
+    for i in range(10):
+        q.enqueue(i)
+    return q
+
+
+def test_size_of_queue(queue):
+    assert len(queue) == NUM_OF_ELEMENTS
+
+
+def test_size_of_queue_after_enqueue(queue):
+    test_value = 11
+    queue.enqueue(test_value)
+    assert len(queue) == NUM_OF_ELEMENTS + 1
+
+
+def test_size_of_queue_after_dequeue(queue):
+    queue.dequeue()
+    assert len(queue) == NUM_OF_ELEMENTS - 1
+
+
+def test_queue_is_empty():
+    q = Queue()
+    assert q.is_empty()
+
+
+def test_empty_queue_after_dequeue():
+    q = Queue()
+    with pytest.raises(Empty):
+        q.dequeue()
+
+
+def test_empty_queue_after_front():
+    q = Queue()
+    with pytest.raises(Empty):
+        q.front()
+
+
+def test_front_with_queue(queue):
+    assert queue.front() == 0
+
+
+def test_empty_queue_with_dequeue(queue):
+    for _ in range(NUM_OF_ELEMENTS):
+        queue.dequeue()
+
+    assert queue.is_empty()
+
+
+def test_return_value_from_dequeue(queue):
+    assert queue.dequeue() == 0
+
+
+def test_str_with_full_queue(queue):
+    assert str(queue) == 'Queue([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])'
+
+
+def test_str_with_empty_queue():
+    q = Queue()
+    assert str(q) == 'Queue([])'
+
+
+def test_str_after_enqueue(queue):
+    test_value = 11
+    queue.enqueue(test_value)
+    assert str(queue) == 'Queue([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11])'
+
+
+def test_str_after_dequeue(queue):
+    queue.dequeue()
+    assert str(queue) == 'Queue([1, 2, 3, 4, 5, 6, 7, 8, 9])'
