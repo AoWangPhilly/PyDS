cf4ab11d67b6de991f6c2ce99440cee885788324
AoWangPhilly <aowangphilly@gmail.com>
2021-09-09 21:50:37 -0400

    Add documentation for queue

--- src/PyDS/Queue.py
+++ src/PyDS/Queue.py
@@ -5,17 +5,33 @@ from PyDS.Error import Empty
 
 @dataclass
 class Queue:
+    """Implementation of Queue ADT"""
     __list: list = field(default_factory=list)
 
     def enqueue(self, value: Any) -> None:
+        """Insertion to the tail of the queue
+
+        :param value: The value inserting to the tail
+        :type value: Any
+        """
         self.__list.append(value)
 
     def dequeue(self) -> Any:
+        """Deletion at the front of the queue
+
+        :return: A value at the front of queue
+        :rtype: Any
+        """
         if self.is_empty():
             raise Empty("Queue is empty")
         return self.__list.pop(0)
 
     def front(self) -> Any:
+        """Gets value at front of queue
+
+        :return: A value at the front of queue
+        :rtype: Any
+        """
         if self.is_empty():
             raise Empty("Queue is empty")
         return self.__list[0]
