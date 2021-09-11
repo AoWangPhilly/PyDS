3ad51f61670e2e463af3bf27db407bb62ad859b0
AoWangPhilly <aowangphilly@gmail.com>
2021-09-10 23:32:48 -0400

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyDS',
+    url='https://github.com/AoWangPhilly/PyDS',
+    author='Ao Wang',
+    author_email='aw3338@drexel.edu',
     version='0.0.1',
     description='Python Data Structures',
     py_modules=['PyDS'],
@@ -9,6 +12,3 @@ setup(
     packages=find_packages(where="src"),
     python_requires=">=3.9.1",
 )
-
-# python setup.py bdist_wheel
-# pip install -e .
