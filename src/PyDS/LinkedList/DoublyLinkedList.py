227154be2b89de07f728d29e7ed81baf51c380cc
AoWangPhilly <aowangphilly@gmail.com>
2021-09-12 14:12:58 -0400

    Write methods for DoublyLinkedList

--- src/PyDS/LinkedList/DoublyLinkedList.py
+++ src/PyDS/LinkedList/DoublyLinkedList.py
@@ -4,14 +4,39 @@ from dataclasses import dataclass
 from PyDS.Error import Empty
 from typing import Any
 
+
 @dataclass
 class Node:
     value: Any
     next: Node
     prev: Node
 
+
 @dataclass
 class DoublyLinkedList:
     head: Node
     tail: Node
 
+    def prepend(self, value: Any) -> None:
+        ...
+
+    def append(self, value: Any) -> None:
+        ...
+
+    def search(self, target: Any) -> int:
+        ...
+    
+    def remove_front(self):
+        ...
+
+    def remove_end(self):
+        ...
+
+    def remove(self):
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def __str__(self) -> str:
+        ...
