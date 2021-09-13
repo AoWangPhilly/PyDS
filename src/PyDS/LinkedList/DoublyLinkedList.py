f94421ecc0358f16a1104a77f868eeac1e4f5578
AoWangPhilly <aowangphilly@gmail.com>
2021-09-13 19:53:51 -0400

    Write methods for size, str, and empty

--- src/PyDS/LinkedList/DoublyLinkedList.py
+++ src/PyDS/LinkedList/DoublyLinkedList.py
@@ -31,19 +31,38 @@ class DoublyLinkedList:
         self.size += 1
 
     def append(self, value: Any) -> None:
-        ...
+        node = Node(value, None, None)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+
+        self.size += 1
 
     def search(self, target: Any) -> int:
-        ...
+        tmp = self.head
+        i = 0
+        while tmp:
+            if tmp.value == target:
+                return i
+            tmp = tmp.next
+            i += 1
+        return i
 
     def remove_front(self):
-        ...
-
+        if self.is_empty():
+            raise Empty('Doubly Linked List is empty')
+        
     def remove_end(self):
-        ...
+        if self.is_empty():
+            raise Empty('Doubly Linked List is empty')
 
     def remove(self):
-        ...
+        if self.is_empty():
+            raise Empty('Doubly Linked List is empty')
 
     def is_empty(self):
         return self.size == 0
@@ -65,8 +84,9 @@ class DoublyLinkedList:
         return output
 
 if __name__ == '__main__':
+    from random import randint
     ll = DoublyLinkedList()
-    for i in range(10):
-        ll.prepend(i)
+    for i in [49, 24, 95, 89, 39, 98, 44, 10, 14]:
+        ll.append(i)
 
-    print(ll)
+    print(ll.search(14))
